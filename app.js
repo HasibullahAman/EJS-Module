8474f0bba9a349c5c8494dcf0c471c78ef38d3de
Hasibullah Aman <hasib.aman7@gmail.com>
2023-04-27 04:58:22 +0430

    befor ejs

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ const app = express();
 
 app.get('/', function(req, res) {
     var today = new Date();
+    var curentday = today.getDay();
 
-    if (today.getDay() === 6 || today.getDay() === 0) {
+    if (curentday === 6 || curentday === 0) {
         res.send("Hey I'm Happy today because it's free day")
     } else {
-        res.send("Oh my god I have to work!!!!!!!!!!!!!!!!")
+        res.sendFile(__dirname + "/index.html")
     }
 
 
