00e01a33bfa11c638e26da535d48bfc89c0d8e3c
Hasibullah Aman <hasib.aman7@gmail.com>
2023-05-03 17:09:58 +0430

    make a conection with mongodb database

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -9,3 +9,21 @@ const url = 'mongodb://localhost:27017';
 
 
 // Database Name
+const dbname = 'myproject';
+
+
+// Create a new mongoClient
+
+const client = new mongoClient(url);
+
+// Use connet method to connect the server
+
+client.connect(function(err) {
+    assert.equal(null, err);
+    console.log("connect with server wass successfuly!");
+
+
+    const db = client.db(dbName);
+
+    client.close();
+})
